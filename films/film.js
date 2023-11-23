e59a24c3242547457fbec227ab711d2aede0d745
johnny <ionutsbotnaru@gmail.com>
2023-11-23 22:14:09 +0200

    add modal when clicking location showcase photos

--- films/film.js
+++ films/film.js
@@ -1,6 +1,6 @@
-const bunkerVideo = '<iframe width="" height="" src="https://www.youtube.com/embed/3u7LlxhrFyo?si=sxevnfQin2KEDm6Z" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>';
-const hillsVideo = '<iframe width="" height="" src="https://www.youtube.com/embed/eWgnWaYzt9g?si=22jGETXrerGRmrDy" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>';
-const gorgeVideo = '<iframe width="" height="" src="https://www.youtube.com/embed/yNBuJxo-GBk?si=EXz5NZxcl4tiwVC6" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>';
+const bunkerVideo = '<iframe width="" height="" src="https://www.youtube.com/embed/3u7LlxhrFyo?si=sxevnfQin2KEDm6Z" title="YouTube video player" frameborder="0" allow="" allowfullscreen></iframe>';
+const hillsVideo = '<iframe width="" height="" src="https://www.youtube.com/embed/eWgnWaYzt9g?si=22jGETXrerGRmrDy" title="YouTube video player" frameborder="0" allow="" allowfullscreen></iframe>';
+const gorgeVideo = '<iframe width="" height="" src="https://www.youtube.com/embed/yNBuJxo-GBk?si=EXz5NZxcl4tiwVC6" title="YouTube video player" frameborder="0" allow="" allowfullscreen></iframe>';
 
 const videoFrameOne = document.querySelector('.location.one>.location-media>.media-video');
 const vidoeFrameTwo = document.querySelector('.location.two>.location-media>.media-video');
@@ -9,3 +9,15 @@ const vidoeFrameThree = document.querySelector('.location.three>.location-media>
 videoFrameOne.innerHTML = bunkerVideo;
 vidoeFrameTwo.innerHTML = hillsVideo;
 vidoeFrameThree.innerHTML = gorgeVideo;
+
+//location showcase photo gallery control
+const modal = document.querySelector('#modal-photo-showcase');
+const mediaPhotos = document.querySelectorAll('.media-photo');
+mediaPhotos.forEach(photo => {
+    photo.addEventListener('click', () => {
+        let img = `<img src="../assets/films-assets/photo-F-${photo.dataset.order}.png" alt=""></img>`;
+        modal.innerHTML = '';
+        modal.innerHTML += img;
+        modal.show()
+    })
+})
