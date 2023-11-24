3aa3b7b607163ef16f6b496d90d6288cae23f8ed
johnny <ionutsbotnaru@gmail.com>
2023-11-24 22:41:25 +0200

    add close button to photo gallery

--- films/film.js
+++ films/film.js
@@ -20,13 +20,21 @@ mediaPhotos.forEach(photo => {
     photo.addEventListener('click', () => {
         modal.show();
         imgDiv.style.backgroundImage = `url('../assets/films_assets/photo-F-${photo.dataset.order}.png')`;
-        modalInnerContainer.innerHTML = '';
+        if (modalInnerContainer.firstChild !== modalInnerContainer.lastChild) {
+            modalInnerContainer.removeChild(modalInnerContainer.lastChild);
+        }
         modalInnerContainer.appendChild(imgDiv);
         modal.blur();
         if (parseInt(photo.dataset.order) <= 3) {
-            modalInnerContainer.style.top = "0";
+            modal.style.top = "0";
         } else if (parseInt(photo.dataset.order) > 3 && parseInt(photo.dataset.order) <=6) {
-            modalInnerContainer.style.top = "33%";
-        } else {modalInnerContainer.style.top = "66%"}
+            modal.style.top = "34%";
+        } else {modal.style.top = "66%"}
     })
+})
+
+const closeButton = document.querySelector('.close');
+closeButton.innerHTML = '<svg viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg" fill=""><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path fill="" d="M195.2 195.2a64 64 0 0 1 90.496 0L512 421.504 738.304 195.2a64 64 0 0 1 90.496 90.496L602.496 512 828.8 738.304a64 64 0 0 1-90.496 90.496L512 602.496 285.696 828.8a64 64 0 0 1-90.496-90.496L421.504 512 195.2 285.696a64 64 0 0 1 0-90.496z"></path></g></svg>';
+closeButton.addEventListener('click', () => {
+    modal.close();
 })
