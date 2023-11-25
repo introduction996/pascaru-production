85ec66edf6a5b2d123c2d59e4d19dd4dd2173859
johnny <ionutsbotnaru@gmail.com>
2023-11-25 15:27:02 +0200

    add photo showcase arrows functionality

--- films/film.js
+++ films/film.js
@@ -32,7 +32,7 @@ mediaPhotos.forEach(photo => {
         } else {modal.style.setProperty('--top-value', '66%');}
 
         modal.show();
-        index = photo.dataset.order;
+        index = parseInt(photo.dataset.order);
     })
 })
 
@@ -48,14 +48,66 @@ arrowL.innerHTML = '<svg viewBox="0 0 24 24" fill="" xmlns="http://www.w3.org/20
 arrowR.innerHTML = '<svg viewBox="0 0 24 24" fill="" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M9.71069 18.2929C10.1012 18.6834 10.7344 18.6834 11.1249 18.2929L16.0123 13.4006C16.7927 12.6195 16.7924 11.3537 16.0117 10.5729L11.1213 5.68254C10.7308 5.29202 10.0976 5.29202 9.70708 5.68254C9.31655 6.07307 9.31655 6.70623 9.70708 7.09676L13.8927 11.2824C14.2833 11.6729 14.2833 12.3061 13.8927 12.6966L9.71069 16.8787C9.32016 17.2692 9.32016 17.9023 9.71069 18.2929Z" fill=""></path> </g></svg>';
 arrowR.addEventListener('click', () => {
     const image = document.querySelector('#img');
-    image.style.backgroundImage = `url('../assets/films_assets/photo-F-${index++}.png')`;
     if (index <= 3) {
-        // if ((index+1) > 3) {
-        //     index = 1
-        //     image.style.backgroundImage = `url('../assets/films_assets/photo-F-${index}.png')`;
-        // } else {
-        //     index += 1
-        //     image.style.backgroundImage = `url('../assets/films_assets/photo-F-${index}.png')`;
-        // }
+        if ((index+1) > 3) {
+            index = 1;
+            image.style.backgroundImage = `url('../assets/films_assets/photo-F-${index}.png')`;
+        } else {
+            index += 1;
+            image.style.backgroundImage = `url('../assets/films_assets/photo-F-${index}.png')`;
+        }
+    }
+
+    if (index > 3 && index <= 6) {
+        if ((index+1) > 6) {
+            index = 4;
+            image.style.backgroundImage = `url('../assets/films_assets/photo-F-${index}.png')`;
+        } else {
+            index += 1;
+            image.style.backgroundImage = `url('../assets/films_assets/photo-F-${index}.png')`;
+        }
+    }
+
+    if (index > 6) {
+        if ((index+1) > 9) {
+            index = 7;
+            image.style.backgroundImage = `url('../assets/films_assets/photo-F-${index}.png')`;
+        } else {
+            index += 1;
+            image.style.backgroundImage = `url('../assets/films_assets/photo-F-${index}.png')`;
+        }
+    }
+});
+
+arrowL.addEventListener('click', () => {
+    const image = document.querySelector('#img');
+    if (index <= 3) {
+        if ((index-1) < 1) {
+            index = 3;
+            image.style.backgroundImage = `url('../assets/films_assets/photo-F-${index}.png')`;
+        } else {
+            index -= 1;
+            image.style.backgroundImage = `url('../assets/films_assets/photo-F-${index}.png')`;
+        }
+    }
+
+    if (index > 3 && index <= 6) {
+        if ((index-1) < 4) {
+            index = 6;
+            image.style.backgroundImage = `url('../assets/films_assets/photo-F-${index}.png')`;
+        } else {
+            index -= 1;
+            image.style.backgroundImage = `url('../assets/films_assets/photo-F-${index}.png')`;
+        }
+    }
+
+    if (index > 6) {
+        if ((index-1) < 7) {
+            index = 9;
+            image.style.backgroundImage = `url('../assets/films_assets/photo-F-${index}.png')`;
+        } else {
+            index -= 1;
+            image.style.backgroundImage = `url('../assets/films_assets/photo-F-${index}.png')`;
+        }
     }
 })
